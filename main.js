662ffd0a49370e41937135cd7c48603c83a93cdd
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-28 17:41:48 +0100

    done

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log('Hello node js!');
+
+const thepin = process.env.PIN;
+console.log(`my PIN is: ${thepin}`);
+
+// nelvy@Legion5 MINGW64 /d/JAVASCRIPT(JS)_Projects/BOOLEAN_JAVASCRIPT_gits/OOPfurther/node-hello-world (main)
+// $ node main.js
+// Hello node js!
+
+// nelvy@Legion5 MINGW64 /d/JAVASCRIPT(JS)_Projects/BOOLEAN_JAVASCRIPT_gits/OOPfurther/node-hello-world (main)
+// $ PIN=666 node main.js
+// Hello node js!
+
+// nelvy@Legion5 MINGW64 /d/JAVASCRIPT(JS)_Projects/BOOLEAN_JAVASCRIPT_gits/OOPfurther/node-hello-world (main)
+// $ PIN=666 node main.js
+// Hello node js!
+// my PIN is: 666
